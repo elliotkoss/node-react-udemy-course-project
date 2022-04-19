e7b8f923e844828ce3147e3278318eab35c03be6
elliotkoss <elliotkoss@gmail.com>
2022-04-19 14:05:26 -0500

    payments

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 
 //can add or remove properties of the schema without issue
 const userSchema = new Schema({
-    googleId: String
+    googleId: String,
+    credits: { type: Number, default: 0}
 });
 
 //2 inputs means we're putting into mongo
