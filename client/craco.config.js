ef49b9c0957bb2c3d3c109d6e69c8dfff07a02c5
sealocal <4951910+sealocal@users.noreply.github.com>
2022-04-25 16:19:18 -0700

    add config to suprress source map warnings

--- client/craco.config.js
+++ client/craco.config.js
@@ -20,6 +20,16 @@ module.exports = {
                     buffer: require.resolve('buffer'),
                 },
 	        },
+            ignoreWarnings: [
+                function ignoreSourcemapsloaderWarnings(warning) {
+                  return (
+                    warning.module &&
+                    warning.module.resource.includes("node_modules") &&
+                    warning.details &&
+                    warning.details.includes("source-map-loader")
+                  );
+                },
+            ]
 	    },
     },
 }
